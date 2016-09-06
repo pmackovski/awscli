51fc4a2660aea842b1d043630f9f5b580508f911
pmackovski <phil.mackovski@servicemesh.com>
2016-09-06 11:19:41 +1000

    removed reference to aws signin URL. Added comments

--- createIAMuser.sh
+++ createIAMuser.sh
@@ -1,30 +1,22 @@
-#createIAMuser.sh
-#Phil Mackovski
-
-#Creates a IAM user account in AWS
+# 1. Creates an IAM user account in AWS
+# 2. Randomly generates a password for the account using apg
+# 3. Adds the user to the Administrators group
+# 4. Creates a login profile for the user (set password and set flag to force user to reset password upon initial logon)
 
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-    echo "Usage: createIAMuser.sh <username>"
+    echo "Usage: $0 <username>"
     exit 1
 fi
 
 USERNAME=$1
 
-#use apg tool to generate random password
+#Generate random password
 PASSWD=$(apg -n 1 -m 12 -x 12 -M SNCL -c cl_seed -a 1)
 
-#create user
 aws iam create-user --user-name $USERNAME
-
-#add the user to a group
 aws iam add-user-to-group --group-name Administrators --user-name $USERNAME
-
-#create a login profile for the user (set password and set flag to enforce password reset upon initial logon)
 aws iam create-login-profile --user-name $USERNAME --password $PASSWD --password-reset-required
 
-#send below to requestor
-printf "\nURL:      https://proserv-au.signin.aws.amazon.com/console\n"
-printf "username: $USERNAME\n"
-printf "password: $PASSWD\n\n"
+echo Password: $PASSWD
